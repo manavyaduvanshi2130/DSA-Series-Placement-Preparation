312d57716f8f66ec22830a598143bbb95dd1189e
Rahul <rahul@example.com>
2026-08-22 11:36:41 +0530

    3 patterns solve in hard level

---
+++ .history/DAY6/SUPER HARD PATTERN/lect/3_20260822112040.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ; 
+    cin>>n;
+    int i , j;
+    for(i = n ; i>= 1 ; i--){
+        for(j = i ; j<=n-i ; j++){
+            cout<<"  ";
+        }
+        for(j = 1 ; j<= 2*i-1 ; j++){
+            cout<<" *";
+        }
+        cout<<endl;
+    }
+}
