5ccc7879e2dbdee0c2b3119af10c48b590d37d64
Rahul <rahul@example.com>
2026-08-22 11:06:52 +0530

    day 6 1st patttern

---
+++ .history/DAY6/SUPER HARD PATTERN/lect/1_20260822110119.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ; 
+    cin>>n;
+    int i , j;
+    for(i = 1 ; i<= n ; i++){
+        for(j = 1 ; j<=n-1 ; j++){
+            cout<<"  ";
+        }
+        for(j = 1 ; j<= 2*i-1 ; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
